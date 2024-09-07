f624ec5fcfc353d02acb35a9f036d92c02be0b0b
jon <phompasit2323@gmail.com>
2024-09-07 20:47:20 +0700

    jja

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 const methodOverride = require("method-override");
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 const ms = require("ms");
-const productsRouter = require('./routes/products');
+const productsRouter = require("./routes/products");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const notFoundMiddleware = require('./middleware/not-found');
-const errorMiddleware = require('./middleware/error-handler');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const session = require("express-session");
 const { createClient } = require("redis");
-const RedisStore = require("connect-redis").default; 
+const RedisStore = require("connect-redis").default;
 
 const corsOptions = {
-  origin: ["https://front-admin-pi.vercel.app"],
+  origin: [`${process.env.FRONTEND_URL}`],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -31,21 +31,21 @@ const setupMiddleware = async () => {
       url: process.env.REDIS_URL,
       legacyMode: true,
     });
-  
+
     redisClient.on("error", (err) => {
       console.error("Redis Client Error", err);
     });
-  
+
     try {
       await redisClient.connect();
       console.log("Connected to Redis");
     } catch (err) {
       console.error("Error connecting to Redis:", err);
     }
-  
+
     return redisClient;
   };
-  
+
   const setupSession = (redisClient) => {
     return new RedisStore({
       client: redisClient,
@@ -68,9 +68,9 @@ const setupMiddleware = async () => {
     resave: false,
     saveUninitialized: false,
   };
-  
+
   // Middleware
-  const uploadDir = path.join(__dirname, 'uploads');
+  const uploadDir = path.join(__dirname, "uploads");
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
@@ -80,11 +80,11 @@ const setupMiddleware = async () => {
   app.use(session(sessionOptions));
   app.use(cookieParser("ab231"));
   app.use(methodOverride("_method"));
-  app.use('/uploads', express.static(uploadDir));
-  app.use('/images', express.static(path.join(__dirname, 'public/images')));
+  app.use("/uploads", express.static(uploadDir));
+  app.use("/images", express.static(path.join(__dirname, "public/images")));
 
   // Routes
-  app.use('/api/v1/products', productsRouter);
+  app.use("/api/v1/products", productsRouter);
 
   // Error handling
   app.use(notFoundMiddleware);
@@ -92,7 +92,7 @@ const setupMiddleware = async () => {
 
   try {
     await connectDB(process.env.MONGODB_URL);
-    
+
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("DB connection error:", error);
@@ -100,9 +100,12 @@ const setupMiddleware = async () => {
   }
 };
 
-setupMiddleware().catch(error => {
+setupMiddleware().catch((error) => {
   console.error("Setup Error:", error);
   process.exit(1);
-});;
-
+});
 module.exports = app;
+// const PORT = 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
